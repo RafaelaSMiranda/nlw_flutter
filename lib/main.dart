4269df0c04a18e4b5786e6f4c709aa4e6f313f56
Rafaela <rafaela.silva.m@outlook.com>
2021-06-25 20:38:37 -0300

    AULA 2 NLW

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,20 @@
+import 'package:nlw_flutter/modulos/splash/splash_page.dart';
+import 'package:nlw_flutter/shared/themes/app_colors.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.purple), home: HomePage()));
+  runApp(AppWidget());
 }
 
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  var count = 0;
-
-  void increment() {
-    count++;
-    setState(() {});
-  }
-
+class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Meu primeiro App'),
+    return MaterialApp(
+      title: 'Pay Flow',
+      theme: ThemeData(
+        primaryColor: AppColors.primary,
       ),
-      body: Center(
-          child: Text(
-        "Contador\n$count",
-        textAlign: TextAlign.center,
-      )),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          increment();
-        },
-      ),
+      home: SplashPage(),
     );
   }
 }
