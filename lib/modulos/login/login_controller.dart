f31c9d1a2ca72e93047d2213641a0da026e1dfb4
Rafaela <rafaela.silva.m@outlook.com>
2021-06-28 22:36:00 -0300

    Aula 3 continuação

--- lib/modulos/login/login_controller.dart
+++ lib/modulos/login/login_controller.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:nlw_flutter/shared/auth/auth_controller.dart';
+import 'package:nlw_flutter/shared/models/user_model.dart';
 
 class LoginController {
   final authController = AuthController();
@@ -12,7 +13,9 @@ class LoginController {
     );
     try {
       final response = await _googleSignIn.signIn();
-      authController.setUser(context, response);
+      final user =
+          UserModel(name: response!.displayName!, photoURL: response.photoUrl);
+      authController.setUser(context, user);
     } catch (error) {
       authController.setUser(context, null);
       print(error);
--- lib/shared/auth/auth_controller.dart
+++ lib/shared/auth/auth_controller.dart
@@ -1,23 +1,38 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:nlw_flutter/modulos/home/home_page.dart';
-import 'package:nlw_flutter/modulos/login/login_page.dart';
+import 'package:nlw_flutter/shared/models/user_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthController {
-  var _isAuthenticated = false;
-  var _user;
+  UserModel? _user;
 
-  get user => _user;
+  UserModel get user => _user!;
   // NÃO CONSEGUE MODIFICAR A VARÍAVEL EM OUTRO LOCAL DO CODIGO
 
-  void setUser(BuildContext context, var user) {
+  void setUser(BuildContext context, UserModel? user) {
     if (user != null) {
-      _isAuthenticated = true;
+      saveUser(user);
       _user = user;
       Navigator.pushReplacementNamed(context, "\home");
     } else {
-      _isAuthenticated = false;
       Navigator.pushReplacementNamed(context, "\login");
+    }
+  }
+
+  Future<void> saveUser(UserModel user) async {
+    final instance = await SharedPreferences.getInstance();
+    await instance.setString("user", user.toJson());
+    return;
+  }
+
+  Future<void> currentUser(BuildContext context) async {
+    final instance = await SharedPreferences.getInstance();
+    if (instance.containsKey("user")) {
+      final json = instance.get("user") as String;
+      setUser(context, UserModel.fromJson(json));
+      return;
+    } else {
+      setUser(context, null);
     }
   }
 }
